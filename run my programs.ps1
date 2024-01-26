d37820971a713850c62af56c28353b0d84c50e55
nayefserag <nayfserag5@gmail.com>
2024-01-26 23:05:15 +0200

    Run Postman , MongoDBCompoass and edge

---
+++ run my programs.ps1
@@ -0,0 +1,8 @@
+# Open Postman
+Start-Process "C:\Users\nayfs\AppData\Local\Postman\Postman.exe"
+
+# Open MongoDB Compass
+Start-Process "C:\Users\nayfs\AppData\Local\MongoDBCompass\MongoDbCompass.exe"
+
+# Open Microsoft Edge
+Start-Process "msedge"
